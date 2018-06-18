3dee9f8bcb939d93e1ec7fbb5f7d6cec42111f26
FolaShocky <34408186+FolaShocky@users.noreply.github.com>
2018-06-18 13:28:50 +0100

    Update OptionsRecylerViewAdapter.java

--- OptionsRecylerViewAdapter.java
+++ OptionsRecylerViewAdapter.java
@@ -15,12 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-/**
- * Created by folas on 27/02/2018.
- */
-
-public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecylerViewAdapter.TimeViewHolder>
-{
+public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecylerViewAdapter.TimeViewHolder> {
 
     private int count = 0;
     private Context context;
@@ -28,22 +23,19 @@ public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecyl
     public static List<Integer>TimeLimit = new ArrayList<>(1);
 
     private RadioButton previousRadioButton;
-    public OptionsRecylerViewAdapter(Context context, List<Time> timesList)
-    {
+    public OptionsRecylerViewAdapter(Context context, List<Time> timesList) {
         this.context = context;
         this.timesList = timesList;
         TimeLimit.add(OptionsActivity.sharedPreferences.getInt("minutes",1));
     }
     @Override
-    public TimeViewHolder onCreateViewHolder(ViewGroup parent,int viewType)
-    {
+    public TimeViewHolder onCreateViewHolder(ViewGroup parent,int viewType) {
         View ItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_option_content,parent,false);
         ItemView.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         return new TimeViewHolder(ItemView);
     }
     @Override
-   public void onBindViewHolder(final TimeViewHolder timeViewHolder, final int position)
-   {
+   public void onBindViewHolder(final TimeViewHolder timeViewHolder, final int position) {
 
        timeViewHolder.setIsRecyclable(false);
        final int retrievedMinutes = OptionsActivity.sharedPreferences.getInt("minutes",1);
@@ -54,46 +46,36 @@ public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecyl
 
        timeViewHolder.txtMins.setText(String.valueOf(timesList.get(position).getMinutes()));
        timeViewHolder.txtMinWord.setText(R.string.minuteword);
-       if(timesList.get(position).getMinutes()<=1)
-       {
+       if(timesList.get(position).getMinutes()<=1) {
            timeViewHolder.txtMinWord.setText(R.string.singleminute);
        }
-       timeViewHolder.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
-       {
+       timeViewHolder.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
            @Override
-           public void onCheckedChanged(RadioGroup group, int checkedId)
-           {
+           public void onCheckedChanged(RadioGroup group, int checkedId) {
                RadioButton checkedRadioButton=(RadioButton)group.findViewById(checkedId);
                Random random = new Random();
                int num = random.nextInt(Integer.MAX_VALUE);
-               if(previousRadioButton !=null)
-               {
+               if(previousRadioButton !=null) {
                    previousRadioButton.setChecked(false);
                    previousRadioButton.setId(num);
                }
                previousRadioButton = checkedRadioButton;
-               if(!previousRadioButton.isChecked())
-               {
+               if(!previousRadioButton.isChecked()) {
                    timesList.get(position).setChecked(false);
                    timeViewHolder.txtTime.setTextColor(Color.BLACK);
                    timeViewHolder.txtMins.setTextColor(Color.BLACK);
                    timeViewHolder.txtMinWord.setTextColor(Color.BLACK);
                    timeViewHolder.relativeLayout.setBackgroundColor(Color.WHITE);
                }
-
-
            }
 
        });
 
-       timeViewHolder.relativeLayout.setOnClickListener(new View.OnClickListener()
-       {
+       timeViewHolder.relativeLayout.setOnClickListener(new View.OnClickListener() {
            @Override
-           public void onClick(View v)
-           {
+           public void onClick(View v) {
 
-               if(TimeLimit.size() == 0)
-               {
+               if(TimeLimit.size() == 0) {
                    timeViewHolder.radioButtonZero.setChecked(true);
                    timesList.get(timeViewHolder.getAdapterPosition()).setMinutes(timeViewHolder.getAdapterPosition() + 1);
                    TimeLimit.add(timesList.get(timeViewHolder.getAdapterPosition()).getMinutes());
@@ -103,8 +85,7 @@ public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecyl
                    timeViewHolder.relativeLayout.setBackgroundColor(Color.BLACK);
                    Time.setPosition(position);
                }
-               else
-               {
+               else {
 
                    timeViewHolder.radioButtonZero.setChecked(true);
                    TimeLimit.clear();
@@ -119,13 +100,10 @@ public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecyl
            }
        });
 
-       timeViewHolder.radioButtonZero.setOnClickListener(new View.OnClickListener()
-       {
+       timeViewHolder.radioButtonZero.setOnClickListener(new View.OnClickListener() {
            @Override
-           public void onClick(View v)
-           {
-               if(TimeLimit.size() == 0)
-               {
+           public void onClick(View v) {
+               if(TimeLimit.size() == 0) {
                    timesList.get(timeViewHolder.getAdapterPosition()).setMinutes(timeViewHolder.getAdapterPosition() + 1);
                    TimeLimit.add(Integer.parseInt(timeViewHolder.txtMins.getText().toString()));
 
@@ -136,8 +114,7 @@ public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecyl
                    timesList.get(position).setChecked(true);
                    Time.setPosition(position);
                }
-               else
-               {
+               else {
                    TimeLimit.clear();
                    timesList.get(timeViewHolder.getAdapterPosition()).setMinutes(timeViewHolder.getAdapterPosition()+1);
                    TimeLimit.add(Integer.parseInt(timeViewHolder.txtMins.getText().toString()));
@@ -152,33 +129,28 @@ public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecyl
            }
        });
 
-       if(timesList.get(position).getMinutes() == TimeLimit.get(0))
-       {
+       if(timesList.get(position).getMinutes() == TimeLimit.get(0)) {
            timesList.get(position).setChecked(true);
            Time.setPosition(position);
        }
-       else
-       {
+       else {
           timesList.get(position).setChecked(false);
 
        }
 
 
         if(timeViewHolder.txtMins.getCurrentTextColor()==Color.WHITE && timeViewHolder.txtTime.getCurrentTextColor()==Color.WHITE
-                && timeViewHolder.txtMinWord.getCurrentTextColor()==Color.WHITE&&position == Time.getPosition())
-        {
+                && timeViewHolder.txtMinWord.getCurrentTextColor()==Color.WHITE && position == Time.getPosition()) {
             timeViewHolder.radioButtonZero.setChecked(true);
         }
-        if(timesList.get(position).getChecked()&&position == Time.getPosition())
-        {
+        if(timesList.get(position).getChecked()&&position == Time.getPosition()) {
             timeViewHolder.radioButtonZero.setChecked(true);
             timeViewHolder.txtTime.setTextColor(Color.WHITE);
             timeViewHolder.txtMins.setTextColor(Color.WHITE);
             timeViewHolder.txtMinWord.setTextColor(Color.WHITE);
             timeViewHolder.relativeLayout.setBackgroundColor(Color.BLACK);
         }
-        else if (!timesList.get(position).getChecked())
-        {
+        else if (!timesList.get(position).getChecked()) {
             timeViewHolder.txtTime.setTextColor(Color.BLACK);
             timeViewHolder.txtMins.setTextColor(Color.BLACK);
             timeViewHolder.txtMinWord.setTextColor(Color.BLACK);
@@ -187,27 +159,21 @@ public class OptionsRecylerViewAdapter extends RecyclerView.Adapter<OptionsRecyl
    }
 
     @Override
-    public int getItemCount()
-    {
+    public int getItemCount() {
         return timesList.size();
     }
 
-    class TimeViewHolder extends RecyclerView.ViewHolder
-    {
+    class TimeViewHolder extends RecyclerView.ViewHolder {
         TextView txtMins, txtTime,txtMinWord;
         RadioGroup radioGroup;
         RadioButton radioButtonZero;
         RelativeLayout relativeLayout;
-        TimeViewHolder(View view)
-        {
+        TimeViewHolder(View view) {
             super(view);
-
-
             relativeLayout = (RelativeLayout)view.findViewById(R.id.relLayout_option_content);
             txtTime = (TextView)view.findViewById(R.id.txtTime);
             txtMins = (TextView)view.findViewById(R.id.txtMins);
             txtMinWord = (TextView)view.findViewById(R.id.txtMinWord);
-
             radioGroup = (RadioGroup) view.findViewById(R.id.rgParent);
             radioButtonZero =(RadioButton)view.findViewById(R.id.rButton0);
             txtTime.setTextColor(Color.BLACK);
