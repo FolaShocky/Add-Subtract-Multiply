077b522f76e480b36ca1e1040d9ba155198d2a81
FolaShocky <34408186+FolaShocky@users.noreply.github.com>
2018-06-18 13:50:40 +0100

    Update VerticalSpaceOptionsRV.java

--- VerticalSpaceOptionsRV.java
+++ VerticalSpaceOptionsRV.java
@@ -4,21 +4,14 @@ import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-/**
- * Created by folas on 26/03/2018.
- */
-
-public class VerticalSpaceOptionsRV extends RecyclerView.ItemDecoration
-{
+public class VerticalSpaceOptionsRV extends RecyclerView.ItemDecoration {
     private final int verticalSpacing;
 
-    public VerticalSpaceOptionsRV(int verticalSpacing)
-    {
+    public VerticalSpaceOptionsRV(int verticalSpacing) {
         this.verticalSpacing = verticalSpacing;
     }
     @Override
-    public void getItemOffsets(Rect outRect, View view ,RecyclerView parent,RecyclerView.State state)
-    {
+    public void getItemOffsets(Rect outRect, View view ,RecyclerView parent,RecyclerView.State state) {
         outRect.bottom = verticalSpacing;
     }
 }
