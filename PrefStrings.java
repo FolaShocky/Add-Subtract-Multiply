e7194b02d955c4ab416481edf18301b0df726f4d
FolaShocky <34408186+FolaShocky@users.noreply.github.com>
2018-06-18 13:38:12 +0100

    Update PrefStrings.java

--- PrefStrings.java
+++ PrefStrings.java
@@ -1,11 +1,7 @@
 package com.folashocky.add_subtract_multiply;
 
-/**
- * Created by folas on 02/04/2018.
- */
 
-public class PrefStrings
-{
+public class PrefStrings {
     public static final String ADD_EASY_QUESTION_COUNT = "Add|Easy|QuestionCount",
     ADD_MEDIUM_QUESTION_COUNT = "Add|Medium|QuestionCount",
     ADD_HARD_QUESTION_COUNT= "Add|Hard|QuestionCount";
@@ -21,5 +17,4 @@ public class PrefStrings
     public static final String RANDOM_EASY_QUESTION_COUNT="Random|Easy|QuestionCount",
     RANDOM_MEDIUM_QUESTION_COUNT="Random|Medium|QuestionCount",
     RANDOM_HARD_QUESTION_COUNT="Random|Hard|QuestionCount";
-
 }
