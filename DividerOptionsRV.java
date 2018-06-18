3fe1df4e287deb90f7fc439a26a82f8a12cfacda
FolaShocky <34408186+FolaShocky@users.noreply.github.com>
2018-06-18 13:14:37 +0100

    Update DividerOptionsRV.java

--- DividerOptionsRV.java
+++ DividerOptionsRV.java
@@ -8,32 +8,23 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-/**
- * Created by folas on 26/03/2018.
- */
-
-public class DividerOptionsRV extends RecyclerView.ItemDecoration
-{
+public class DividerOptionsRV extends RecyclerView.ItemDecoration {
     private static final int[] Attributes = new int[]{android.R.attr.listDivider};
     private Drawable divider;
 
-    public DividerOptionsRV(Context context)
-    {
+    public DividerOptionsRV(Context context) {
         TypedArray styledAttrs = context.obtainStyledAttributes(Attributes);
         divider = styledAttrs.getDrawable(0);
         styledAttrs.recycle();
     }
-    public DividerOptionsRV(Context context,int resId)
-    {
+    public DividerOptionsRV(Context context,int resId) {
         divider = ContextCompat.getDrawable(context,resId);
     }
-    public void onDraw(Canvas c ,RecyclerView recyclerView,RecyclerView.State state)
-    {
+    public void onDraw(Canvas c ,RecyclerView recyclerView,RecyclerView.State state) {
         int left = recyclerView.getPaddingLeft();
         int right = recyclerView.getWidth()-recyclerView.getPaddingRight();
         int itemCount = recyclerView.getChildCount();
-        for(int i = 0;i<itemCount;i++)
-        {
+        for(int i = 0;i<itemCount;i++) {
             View child = recyclerView.getChildAt(i);
             RecyclerView.LayoutParams params = (RecyclerView.LayoutParams)child.getLayoutParams();
             int top= child.getBottom()+params.bottomMargin;
